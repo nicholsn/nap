f85bae901973167705772fe3f9181253e8bdb553
richstoner <stonerri@gmail.com>
2013-03-27 11:11:54 -0700

    updating fabfile

--- fabfile.py
+++ fabfile.py
@@ -38,8 +38,8 @@ def last():
 
 
 def printssh():
-	print 'ssh -i ~/.ssh/ec2-keypair ubuntu@%s' % (env.host_string)
-	local('echo "ssh -i ~/.ssh/ec2-keypair ubuntu@%s" | pbcopy ' % (env.host_string))
+	print 'ssh -i ~/.ssh/%s.pem ubuntu@%s' % (ec2keypairname, env.host_string)
+	local('echo "ssh -i ~/.ssh/%s.pem ubuntu@%s" | pbcopy ' % (ec2keypairname, env.host_string))
 
 def printhttp():
 	print 'http://%s' % (env.host_string)	
@@ -80,7 +80,7 @@ def _create(size):
 		conn = ec2.EC2Connection(aws_access_key_id, aws_secret_access_key)
 		
 		time.sleep(1)
-		reservation = conn.run_instances(precise_12_04_2, instance_type=size, placement='us-east-1d', key_name='ec2-keypair')
+		reservation = conn.run_instances(precise_12_04_2, instance_type=size, placement='us-east-1d', key_name=ec2keypairname, security_group=['default', 'irodsweb'])
 		time.sleep(1)
 		instance = reservation.instances[0]
 		time.sleep(1)
@@ -128,23 +128,6 @@ def install():
 		_base()
 		_provision()
 		_externals()
-
-
-
-def nap():
-
-	with settings(warn_only=True):
-
-		_notebook()
-		
-		# get the most recent version, or clone to directory
-		if exists('nap'):
-			with cd('nap'):
-				run('git pull --rebase')
-		else:
-			run('git clone git@github.com:ni-/nap.git')				
-			run('git config --global user.name "richstoner"')
-			run('git config --global user.email "stonerri@gmail.com"')
 
 
 
@@ -214,26 +197,18 @@ def unmountebs():
 			print 'volume detached successfully'
 		else:
 			print 'volume not attached successfully'
-
 		print v_to_unmount.attachment_state()
-
-
 
 
 def _base():
 	'''[create] Basic packages for building, version control'''
 	with settings(warn_only=True):
-		
-		# update existing tools
 		run("sudo apt-get -y update", pty = True)
-		# run("sudo apt-get -y upgrade", pty = True)		
-		
-		# install build and CVS tools
-		packagelist = ' '.join(['git-core', 'mercurial', 'subversion', 'unzip', 'build-essential', 'g++', 'libav-tools', 'uuid-dev', 'libfreetype6-dev','libpng12-dev'])
-		
+		run("sudo apt-get -y upgrade", pty = True)		
+
+		packagelist = ' '.join(['git-core', 'mercurial', 'subversion', 'unzip', 'build-essential', 'g++', 'libav-tools', 'uuid-dev', 'libfreetype6-dev','libpng12-dev'])		
 		run('sudo apt-get -y install %s' % packagelist, pty = True)
 
-		# install python components
 		packagelist = ' '.join(['python-setuptools', 'python-pip', 'python-dev', 'python-lxml', 'libxml2-dev', 'python-imaging', 'libncurses5-dev', 'cmake-curses-gui', 'imagemagick', 's3cmd'])
 		run('sudo apt-get -y install %s' % packagelist, pty = True)
 		
@@ -261,8 +236,6 @@ def _enableNeuroDebian():
             run('wget -O- http://neuro.debian.net/lists/precise.us-nh | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list')
 
 
-
-
 def _externals():
 	'''[create] some external dependencies'''
 	with settings(warn_only=True):
@@ -281,33 +254,32 @@ def _externals():
 			sudo('pip install -U scipy')
 			sudo('pip install git+git://github.com/scikit-image/scikit-image.git')
 
-			run('wget http://sourceforge.net/projects/fiji-bi/files/fiji/Madison/fiji-linux64-20110307.tar.bz2/download')
-			run('mv download fiji.tar.bz2')
-			run('tar xvjf fiji.tar.bz2')
 
-			# run('wget http://sourceforge.net/projects/itk/files/itk/3.20/InsightToolkit-3.20.1.tar.gz/download')
-			# run('mv download itk32.tar.gz')
-			# run('tar xvzf itk32.tar.gz')
+def configureRS():
+	put('id_rsa.pub','~/.ssh/id_rsa.pub')
+	put('id_rsa', '~/.ssh/id_rsa')
 
-
+	sudo('chmod 0600 .ssh/id_rsa')
+	sudo('chmod 0600 .ssh/id_rsa.pub')
 
 
-def _notebook():
-	'''install python notebook'''
+def nap():
 	with settings(warn_only=True):
 
-		put('id_rsa.pub','~/.ssh/id_rsa.pub')
-		put('id_rsa', '~/.ssh/id_rsa')
-		
-		sudo('chmod 0600 .ssh/id_rsa')
-		sudo('chmod 0600 .ssh/id_rsa.pub')
+		# get the most recent version, or clone to directory
+		if exists('nap'):
+			with cd('nap'):
+				run('git pull --rebase')
+		else:
+			run('git clone git@github.com:ni-/nap.git')				
+			run('git config --global user.name "richstoner"')
+			run('git config --global user.email "stonerri@gmail.com"')
 
-		# if exists('ipynb'):
-		# 	with cd('ipynb'):
-		# 		run('git pull --rebase')
-		# else:
-		# 	run('git clone git@github.com:richstoner/ipynb.git')
-			
+
+
+def notebook():
+	'''install python notebook'''
+	with settings(warn_only=True):
 
 		sudo('easy_install readline')
 		sudo('apt-get install -y libfreetype6-dev libpng12-dev python-matplotlib')
@@ -316,7 +288,6 @@ def _notebook():
 		sudo('pip install -U beautifulsoup4')
 		sudo('pip install pyzmq')
 		sudo('pip install workerpool')
-		# sudo('pip install -U matplotlib')
 
 		run('ipython profile create default')
 
